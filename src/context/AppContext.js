5511d74102109281005500bc52ee90734275d929
Aman Aryan <aman.a@scrollstack.com>
2021-03-13 13:38:23 +0530

    added some more functionality to he app

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -2,18 +2,26 @@ import React, { createContext, useReducer } from "react";
 
 const AppReducer = (state, action) => {
   switch (action.type) {
+    case "ADD_EXPENSE":
+      return {
+        ...state,
+        expenses: [...state.expenses, action.payload],
+      };
+    case "DELETE_EXPENSE":
+      return {
+        ...state,
+        expenses: state.expenses.filter(
+          (expense) => expense.id !== action.payload
+        ),
+      };
     default:
       return state;
   }
 };
 
 const initialState = {
-  budget: 1500,
-  expenses: [
-    { id: 12, name: "shopping", cost: 40 },
-    { id: 13, name: "holiday", cost: 300 },
-    { id: 14, name: "car service", cost: 50 },
-  ],
+  budget: 200,
+  expenses: [],
 };
 
 export const AppContext = createContext();
